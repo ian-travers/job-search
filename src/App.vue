6b786a6ac36fb1d82c449cc1fcc3d530aff7df2e
Ian Travers <mr.itpe@gmail.com>
2022-08-12 10:31:14 +0300

    03.3 A Little Houskeeping [delete all we don't need]

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App. It's named Job Search" />
+  <h1>I'am a Vue JS App</h1>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
