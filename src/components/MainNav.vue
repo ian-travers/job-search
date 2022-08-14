f95531982ccd2781a6f5b90f96a11cd30c9c74a9
Ian Travers <mr.itpe@gmail.com>
2022-08-14 09:44:01 +0300

    05.11 Rendering Data to View II

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,6 +5,9 @@
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
         <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <h2 class="flex items-center h-full ml-8">
+          Developer by {{ author.firstName }} {{ author.lastName }}
+        </h2>
       </div>
     </div>
   </header>
@@ -17,6 +20,10 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
+      author: {
+        firstName: "Boris",
+        lastName: "Paskhaver",
+      },
     };
   },
 };
