534328f46f8d75a27969bf8fe4a243d7809327a1
Ian Travers <mr.itpe@gmail.com>
2022-08-14 09:01:58 +0300

    05.06 CSS Styling the MainNav Component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="w-full text-sm">Main Menu</header>
+  <header class="w-full text-sm">
+    <div class="fixed w-full top-0 left-0 h-16 bg-white"></div>
+  </header>
 </template>
 
 <script>
