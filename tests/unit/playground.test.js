6f17742032abfc724a077c6f190521062ff498c5
Ian Travers <mr.itpe@gmail.com>
2022-08-15 09:37:56 +0300

    06.02 The Basics of Jest Syntax

---
+++ tests/unit/playground.test.js
@@ -0,0 +1,10 @@
+describe("basic math", () => {
+  it("adds two numbers", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it("substact two numbers", () => {
+    expect(10 - 8).toBe(2);
+    expect(4 - 1).toBe(3);
+  });
+});
