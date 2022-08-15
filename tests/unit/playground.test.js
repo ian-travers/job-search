fbbbf928e2dfe7e955f1025c6c009386627cf89a
Ian Travers <mr.itpe@gmail.com>
2022-08-15 09:58:38 +0300

    06.03 Multiple Describe Blocks (VI)

--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,3 +1,5 @@
+import { evenOrOdd } from "@/playground";
+
 describe("basic math", () => {
   it("adds two numbers", () => {
     expect(1 + 1).toBe(2);
@@ -6,5 +8,19 @@ describe("basic math", () => {
   it("substact two numbers", () => {
     expect(10 - 8).toBe(2);
     expect(4 - 1).toBe(3);
+  });
+
+  describe("even or odd", () => {
+    describe("when number is even", () => {
+      it("indicates the number is even", () => {
+        expect(evenOrOdd(4)).toBe("Even");
+      });
+    });
+
+    describe("when number is odd", () => {
+      it("indicates the number is odd", () => {
+        expect(evenOrOdd(5)).toBe("Odd");
+      });
+    });
   });
 });
