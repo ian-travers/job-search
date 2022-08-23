c9287e6b6a281d688054e798c74b6460266925c2
Ian Travers <mr.itpe@gmail.com>
2022-08-23 13:18:04 +0300

    09.03 CSS Styling ActionButton

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button>Sign in</button>
+  <button
+    class="font-medium text-white bg-brand-blue-1 border-0 rounded px-5 py-3"
+  >
+    Sign in
+  </button>
 </template>
 
 <script>
