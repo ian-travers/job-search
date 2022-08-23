7c1181a7da42b1fc466d92e951749258757632eb
Ian Travers <mr.itpe@gmail.com>
2022-08-23 13:28:19 +0300

    09.04 Add Custom Box Shadow to ActionButton (VI)

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="font-medium text-white bg-brand-blue-1 border-0 rounded px-5 py-3"
+    class="font-medium text-white bg-brand-blue-1 border-0 rounded hover:shadow-blue transition px-5 py-3"
   >
     Sign in
   </button>
