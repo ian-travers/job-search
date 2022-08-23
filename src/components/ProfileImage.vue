0a67887df3f8ab75d5f31e93620d40051495ec45
Ian Travers <mr.itpe@gmail.com>
2022-08-23 15:17:49 +0300

    09.05 Create ProfileImage Component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imageUrl" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageUrl:
+        "https://www.pngitem.com/pimgs/m/487-4876417_link-head-png-toon-link-face-png-transparent.png",
+    };
+  },
+};
+</script>
