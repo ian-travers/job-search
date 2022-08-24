8f032293ed8ce0bf8221285b29800c2d2f30affb
Ian Travers <mr.itpe@gmail.com>
2022-08-24 10:28:17 +0300

    09.11 The v-on Directive I HTML Element

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     class="font-medium text-white bg-brand-blue-1 border-0 rounded hover:shadow-blue transition px-5 py-3"
+    v-on:click="handleClick"
   >
     Sign in
   </button>
