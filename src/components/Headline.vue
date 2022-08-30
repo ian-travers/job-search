63fc868ca94d67e3d0b3e0104f0ea21abd8d633a
Ian Travers <mr.itpe@gmail.com>
2022-08-30 10:20:33 +0300

    12.16 Adding Function to Headline Component

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import nextElementInList from "@/utils/nextElementInList";
+
 export default {
   name: "Headline",
 
@@ -40,10 +42,8 @@ export default {
     changeTitle() {
       this.interval = setInterval(() => {
         const actions = ["Build", "Create", "Design", "Code"];
-        const currentActionIndex = actions.indexOf(this.action);
-        const nextActionIndex = (currentActionIndex + 1) % 4;
 
-        this.action = actions[nextActionIndex];
+        this.action = nextElementInList(actions, this.action);
       }, 3000);
     },
   },
