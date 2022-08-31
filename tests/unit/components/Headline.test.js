c2f9b6a83a9928232cb18aa4b805296e74e74e5d
Ian Travers <mr.itpe@gmail.com>
2022-08-31 11:14:54 +0300

    12.17 Mock (VI) [haveBeenCalled]

---
+++ tests/unit/components/Headline.test.js
@@ -0,0 +1,15 @@
+import { mount } from "@vue/test-utils";
+
+import Headline from "@/components/Headline";
+
+describe("Headline", () => {
+  describe("Jest playground", () => {
+    it("tracks whether it has been called", () => {
+      const mockFunction = jest.fn();
+
+      mockFunction();
+
+      expect(mockFunction).toHaveBeenCalled();
+    });
+  });
+});
