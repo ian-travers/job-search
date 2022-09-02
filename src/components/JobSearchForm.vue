5ef032ef8356105f21d11a8a6cdaec085160348b
Ian Travers <mr.itpe@gmail.com>
2022-09-02 15:08:19 +0300

    13.05 CSS Styling JobSearchForm I

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,5 +1,9 @@
 <template>
-  <form>Job search Form Goes Here</form>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+  </form>
 </template>
 
 <script>
