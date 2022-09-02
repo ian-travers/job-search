ac65b2b9f6e6482bca5f34b9d2483635a3b932f5
Ian Travers <mr.itpe@gmail.com>
2022-09-02 13:03:57 +0300

    12.22 beforeEach and afterEach (VI)

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -4,29 +4,27 @@ import { nextTick } from "vue";
 import Headline from "@/components/Headline";
 
 describe("Headline", () => {
-  it("displays introductory action verb", () => {
+  beforeEach(() => {
     jest.useFakeTimers("legacy");
+  });
 
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("displays introductory action verb", () => {
     const wrapper = mount(Headline);
     const actionPhrase = wrapper.find("[data-test='action-phrase']");
 
     expect(actionPhrase.text()).toBe("Build for everyone");
-
-    jest.useRealTimers();
   });
 
   it("changes action verb in a consistent interval", () => {
-    jest.useFakeTimers("legacy");
-
     mount(Headline);
     expect(setInterval).toHaveBeenCalled();
-
-    jest.useRealTimers();
   });
 
   it("swaps action verb after first interval", async () => {
-    jest.useFakeTimers("legacy");
-
     const wrapper = mount(Headline);
 
     jest.runOnlyPendingTimers();
@@ -35,18 +33,12 @@ describe("Headline", () => {
     const actionPhrase = wrapper.find("[data-test='action-phrase']");
 
     expect(actionPhrase.text()).toBe("Create for everyone");
-
-    jest.useRealTimers();
   });
 
   it("removes interval when component disappears", () => {
-    jest.useFakeTimers("legacy");
-
     const wrapper = mount(Headline);
     wrapper.unmount();
 
     expect(clearInterval).toHaveBeenCalled();
-
-    jest.useRealTimers();
   });
 });
