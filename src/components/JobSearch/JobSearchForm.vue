3f5efb10af37618db2ab6d64a3381069f30ec369
Ian Travers <mr.itpe@gmail.com>
2022-09-06 11:30:06 +0300

    14.04-06 Overwriting Parent Data from Emits

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,7 +6,10 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex flex-1 items-center h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <text-input placeholder="Software engineer" />
+        <text-input
+          placeholder="Software engineer"
+          @handle-input="updateRole"
+        />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -14,7 +17,7 @@
       >
       <div class="relative flex flex-1 items-center h-full pl-3">
         <label class="absolute left-0 -top-10">Where?</label>
-        <text-input placeholder="Los Angeles" />
+        <text-input placeholder="Los Angeles" @handle-input="updateLocation" />
       </div>
       <action-button text="Search" type="secondary" class="rounded-r-3xl" />
     </div>
@@ -35,6 +38,16 @@ export default {
       role: "",
       location: "",
     };
+  },
+
+  methods: {
+    updateRole(payload) {
+      this.role = payload;
+    },
+
+    updateLocation(payload) {
+      this.location = payload;
+    },
   },
 };
 </script>
