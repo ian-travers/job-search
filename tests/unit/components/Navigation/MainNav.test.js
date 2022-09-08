b9d172b734dc4883e492acc4e3fe76fa47a7eb3c
Ian Travers <mr.itpe@gmail.com>
2022-09-08 14:56:52 +0300

    15.15 Refactoring Test Suite with Factory Function (VI)

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -3,23 +3,22 @@ import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 import MainNav from "@/components/Navigation/MainNav";
 
 describe("MainNav", () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallowMount(MainNav, {
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
+  const createConfig = () => ({
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
       },
-    });
+    },
   });
 
   it("displays company name", () => {
+    const wrapper = shallowMount(MainNav, createConfig());
+
     expect(wrapper.text()).toMatch("Bobo Careers");
   });
 
   it("displays menu items for navigation", () => {
+    const wrapper = shallowMount(MainNav, createConfig());
     const navigationMenuItems = wrapper.findAll(
       "[data-test='main-nav-list-item']"
     );
@@ -37,6 +36,7 @@ describe("MainNav", () => {
 
   describe("when user is logged out", () => {
     it("prompts user to sign in", () => {
+      const wrapper = shallowMount(MainNav, createConfig());
       const loginButton = wrapper.find("[data-test='login-button']");
 
       expect(loginButton.exists()).toBe(true);
@@ -45,6 +45,7 @@ describe("MainNav", () => {
 
   describe("when user is logged in", () => {
     it("displays user profile image", async () => {
+      const wrapper = shallowMount(MainNav, createConfig());
       let profileImage = wrapper.find("[data-test='profile-image']");
       expect(profileImage.exists()).toBe(false);
 
@@ -56,6 +57,7 @@ describe("MainNav", () => {
     });
 
     it("displays subnav menu with additional info", async () => {
+      const wrapper = shallowMount(MainNav, createConfig());
       let subnav = wrapper.find("[data-test='subnav']");
       expect(subnav.exists()).toBe(false);
 
