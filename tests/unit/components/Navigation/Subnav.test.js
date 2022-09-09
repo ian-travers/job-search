f5794be65850aa5fb0494c327997966e952fab7f
Ian Travers <mr.itpe@gmail.com>
2022-09-09 15:21:14 +0300

    16.04 Using Factory Functions in Subnav Test (VI)

--- tests/unit/components/Navigation/Subnav.test.js
+++ tests/unit/components/Navigation/Subnav.test.js
@@ -3,23 +3,23 @@ import { mount } from "@vue/test-utils";
 import Subnav from "@/components/Navigation/Subnav";
 
 describe("Subnav", () => {
+  const createConfig = (routeName) => ({
+    global: {
+      mocks: {
+        $route: {
+          name: routeName,
+        },
+      },
+      stubs: {
+        FontAwesomeIcon: true,
+      },
+    },
+  });
+
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const $route = {
-        name: "JobResults",
-      };
-
-      const wrapper = mount(Subnav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
-
+      const routeName = "JobResults";
+      const wrapper = mount(Subnav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
 
       expect(jobCount.exists()).toBe(true);
@@ -28,21 +28,8 @@ describe("Subnav", () => {
 
   describe("when user is not on job page", () => {
     it("does NOT displays job count", () => {
-      const $route = {
-        name: "Home",
-      };
-
-      const wrapper = mount(Subnav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
-
+      const routeName = "Home";
+      const wrapper = mount(Subnav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
 
       expect(jobCount.exists()).toBe(false);
