86d532e2dcdbf0fb41c443c860a263ec0a6600ce
Ian Travers <mr.itpe@gmail.com>
2022-09-11 10:48:23 +0300

    16.06 Display the Route ID Dynamically

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="mt-8">Job View</div>
+  <div class="text-center mt-8">Job View</div>
+  <div class="text-center mt-2">{{ currentJobId }}</div>
 </template>
 
 <script>
 export default {
   name: "JobView",
+
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
 };
 </script>
