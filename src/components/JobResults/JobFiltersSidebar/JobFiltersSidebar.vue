884da8b93e65c341dd313b6d1f99282c90121fca
Ian Travers <mr.itpe@gmail.com>
2022-09-12 08:00:55 +0300

    17.04 CSS Styling JobFiltersSidebar and JobListings Components

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="">Job Filters Sidebar</div>
+  <div class="flex flex-col bg-white border-r border-brand-gray-1 w-96 p-4">
+    Job Filters Sidebar
+  </div>
 </template>
 
 <script>
