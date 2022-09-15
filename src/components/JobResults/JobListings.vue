8696fea81f873dec453ffc9e4f0973c2b05f3a53
Ian Travers <mr.itpe@gmail.com>
2022-09-15 18:07:34 +0300

    18.05 Making Axios Request for Jobs in JobListings Component

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -9,11 +9,24 @@
 </template>
 
 <script>
+import axios from "axios";
 import JobListing from "./JobListing.vue";
 
 export default {
   name: "JobListings",
 
   components: { JobListing },
+
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+
+  mounted() {
+    axios.get("http://localhost:3000/jobs").then((response) => {
+      this.jobs = response.data;
+    });
+  },
 };
 </script>
