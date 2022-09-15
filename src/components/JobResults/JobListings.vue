b4fbd942c9cd62d73b2e8718a38ceb94e8c6b9e3
Ian Travers <mr.itpe@gmail.com>
2022-09-15 16:44:09 +0300

    17.07 CSS Adding Job Qualifications to Job Listing

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -19,6 +19,27 @@
               </div>
             </div>
           </div>
+
+          <div class="px-8 py-4">
+            <div class="">
+              <h3 class="mt-1 mb-2">Qualifications:</h3>
+              <div>
+                <ul class="list-disc pl-8">
+                  <li>Bachelor's degree or equivalent practice experience.</li>
+                  <li>5 years experience in program management.</li>
+                  <li>
+                    Experience analyzing data through querying database (e.g.
+                    SQL), using spreadssheet sofrware.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center mt-2">
+              <router-link to="/jobs/results/1" class="text-brand-blue-1"
+                >Expand</router-link
+              >
+            </div>
+          </div>
         </router-link>
       </li>
     </ol>
