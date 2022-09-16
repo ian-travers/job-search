7f08f4dfaf8c9d75384e46ab7a12c471cc71dc9c
Ian Travers <mr.itpe@gmail.com>
2022-09-16 09:22:59 +0300

    18.10 Update Job Listing to Render Job’s Data

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,19 +1,27 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-brand-gray-2 rounded hover:shadow-gray"
     >
       <div class="border-b border-brand-gray-2 mx-8 pt-5 pb-2">
         <h2 class="text-2xl mb-2">
-          Technical Program Manager, Perception, Augmented Reality
+          {{ job.title }}
         </h2>
         <div class="flex align-middle">
           <div class="mr-5">
-            <span>Bobo</span>
+            <span>{{ job.organization }}</span>
           </div>
-          <div class="">
-            <span>San Francisco, CA, USA</span>
+          <div>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ location }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -23,17 +31,17 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's degree or equivalent practice experience.</li>
-              <li>5 years experience in program management.</li>
-              <li>
-                Experience analyzing data through querying database (e.g. SQL),
-                using spreadssheet sofrware.
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="text-center mt-2">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1"
+          <router-link :to="jobPageLink" class="text-brand-blue-1"
             >Expand</router-link
           >
         </div>
