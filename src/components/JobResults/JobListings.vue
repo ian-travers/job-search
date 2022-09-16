faf8d5250c59baca406a7aff929cb1180380df0d
Ian Travers <mr.itpe@gmail.com>
2022-09-16 08:55:22 +0300

    18.07 Use asyncawait Syntax to Fetch Jobs

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -23,10 +23,9 @@ export default {
     };
   },
 
-  mounted() {
-    axios.get("http://localhost:3000/jobs").then((response) => {
-      this.jobs = response.data;
-    });
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/jobs");
+    this.jobs = response.data;
   },
 };
 </script>
