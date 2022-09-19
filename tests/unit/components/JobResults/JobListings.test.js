a1314f793c4e9179c622c4ba791194db02a39fd5
Ian Travers <mr.itpe@gmail.com>
2022-09-19 10:33:56 +0300

    18.16 Create First Tests for JobListings Component (part 1)

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,5 +1,13 @@
+import { shallowMount } from "@vue/test-utils";
 import axios from "axios";
-
 jest.mock("axios");
 
-axios.get();
+import JobListings from "@/components/JobResults/JobListings";
+
+describe("JobListings", () => {
+  it("fetches jobs", () => {
+    shallowMount(JobListings);
+
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+});
