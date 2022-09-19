efe53e9bd1b64cdd012733ccc9248ba194b647c7
Ian Travers <mr.itpe@gmail.com>
2022-09-19 09:25:09 +0300

    18.12 Refactoring JobListingTest (part 1)

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -3,36 +3,28 @@ import { mount, RouterLinkStub } from "@vue/test-utils";
 import JobListing from "@/components/JobResults/JobListing";
 
 describe("JobListing", () => {
+  const createObject = () => ({
+    props: {
+      job: {
+        title: "Vue Developer",
+        organization: "ClearAir",
+      },
+    },
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
+      },
+    },
+  });
+
   it("renders job title", () => {
-    const wrapper = mount(JobListing, {
-      props: {
-        job: {
-          title: "Vue Developer",
-        },
-      },
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-    });
+    const wrapper = mount(JobListing, createObject());
 
     expect(wrapper.text()).toMatch("Vue Developer");
   });
 
   it("renders job organization", () => {
-    const wrapper = mount(JobListing, {
-      props: {
-        job: {
-          organization: "ClearAir",
-        },
-      },
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-    });
+    const wrapper = mount(JobListing, createObject());
 
     expect(wrapper.text()).toMatch("ClearAir");
   });
