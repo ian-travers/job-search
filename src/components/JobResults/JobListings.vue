cbc5005963a3fa466aa69048e8020463f347f11d
Ian Travers <mr.itpe@gmail.com>
2022-09-21 09:55:54 +0300

    19.15 Fixing the Bug (wip console logging)

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -63,6 +63,8 @@ export default {
     nextPage() {
       const nextPage = this.currentPage + 1;
       const maxPage = this.jobs.length / 10;
+      console.log("NEXT PAGE", nextPage);
+      console.log("MAX PAGE", maxPage);
 
       return nextPage <= maxPage ? nextPage : undefined;
     },
