9c91af375eb974d72fac7b2991291c4a1b3c3542
Ian Travers <mr.itpe@gmail.com>
2022-09-26 11:50:16 +0300

    22.05 CSS Styling Accordion Component

--- src/components/Shared/Accordion.vue
+++ src/components/Shared/Accordion.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="">The Accordion</div>
+  <div class="border-b border-brand-gray-2 py-5">
+    <div
+      class="flex flex-wrap items-center justify-between cursor-pointer"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">Organizations</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+
+    <div v-if="isOpen" class="w-full mt-5">Child</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Accordion",
+
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
