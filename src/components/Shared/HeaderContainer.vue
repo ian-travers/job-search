42e51215c5b5c784f620b9f03c06a8acbc221d19
Ian Travers <mr.itpe@gmail.com>
2022-09-28 09:51:10 +0300

    23.02 Named Slots I

---
+++ src/components/Shared/HeaderContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full border-b border-brand-gray-1">
+    <div class="mx-auto text-center my-16">
+      <slot name="title">Sample Title</slot>
+      <slot name="subtitle">Sample Subtitle</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderContainer",
+};
+</script>
