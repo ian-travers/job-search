33ff9bafba062b1c9c094c51a343094746906b7c
Ian Travers <mr.itpe@gmail.com>
2022-09-28 10:10:27 +0300

    23.04 Named Slots II (# sic!)

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,15 @@
 <template>
-  <header-container />
+  <header-container>
+    <template #title>
+      <h1 class="w-full font-normal text-4xl">Teams</h1>
+    </template>
+
+    <template #subtitle>
+      <h2 class="w-full font-light text-2xl my-4">
+        It's awesome working here. Why don't you come join us?
+      </h2>
+    </template>
+  </header-container>
 </template>
 
 <script>
