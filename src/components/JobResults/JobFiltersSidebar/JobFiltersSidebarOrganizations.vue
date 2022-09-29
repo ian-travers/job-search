996d055875a45f49ef6f9e587c59bbae1fa8cd39
Ian Travers <mr.itpe@gmail.com>
2022-09-29 11:08:05 +0300

    25.06 Use v-for to Render Unique Organizations

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -3,25 +3,13 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-wrap">
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-            <label for="Between Vue and Me">Between Vue</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="At Vue Brute" type="checkbox" class="mr-3" />
-            <label for="At Vue Brute">At Vue Brute</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="Halfman" type="checkbox" class="mr-3" />
-            <label for="Halfman">Halfman</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="Star Control" type="checkbox" class="mr-3" />
-            <label for="Star Control">Star Control</label>
+          <li
+            v-for="organization in UNIQUE_ORGANIZATIONS"
+            :key="organization"
+            class="h-8 w-1/2"
+          >
+            <input :id="organization" type="checkbox" class="mr-3" />
+            <label :for="organization">{{ organization }}</label>
           </li>
         </ul>
       </fieldset>
