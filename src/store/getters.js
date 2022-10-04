42dc3a1ab725179ba3f552c0abccd514b073e931
Ian Travers <mr.itpe@gmail.com>
2022-10-04 15:10:20 +0300

    26.14 Applying our Changes to FILTERED_JOBS (VI)

--- src/store/getters.js
+++ src/store/getters.js
@@ -55,21 +55,10 @@ const getters = {
     return state.selectedJobTypes.includes(job.jobType);
   },
 
-  [FILTERED_JOBS](state) {
-    const noSelectedOrganizations = state.selectedOrganizations.length === 0;
-    const noSelectedJobTypes = state.selectedJobTypes.length === 0;
-
+  [FILTERED_JOBS](state, getters) {
     return state.jobs
-      .filter((job) => {
-        if (noSelectedOrganizations) return true;
-
-        return state.selectedOrganizations.includes(job.organization);
-      })
-      .filter((job) => {
-        if (noSelectedJobTypes) return true;
-
-        return state.selectedJobTypes.includes(job.jobType);
-      });
+      .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
+      .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
   },
 };
 
