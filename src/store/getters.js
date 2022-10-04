466519f97122eb37b5bba2e7a59958519c9d5b10
Ian Travers <mr.itpe@gmail.com>
2022-10-04 11:44:54 +0300

    26.12 Getters with Arguments (VI) (wip)

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,8 @@ import {
   FILTERED_JOBS_BY_ORGANIZATIONS,
   FILTERED_JOBS_BY_JOB_TYPES,
   FILTERED_JOBS,
+  INCLUDE_JOB_BY_ORGANIZATION,
+  INCLUDE_JOB_BY_JOB_TYPE,
 } from "@/store/costants";
 
 const getters = {
@@ -39,6 +41,18 @@ const getters = {
     return state.jobs.filter((job) =>
       state.selectedJobTypes.includes(job.jobType)
     );
+  },
+
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
+    if (state.selectedOrganizations.length === 0) return true;
+
+    return state.selectedOrganizations.includes(job.organization);
+  },
+
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state) => (job) => {
+    if (state.selectedJobTypes.length === 0) return true;
+
+    return state.selectedJobTypes.includes(job.jobType);
   },
 
   [FILTERED_JOBS](state) {
