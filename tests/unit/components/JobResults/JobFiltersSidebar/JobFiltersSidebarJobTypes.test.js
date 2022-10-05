b9a499936e4f7476c04d5fb81151c7c045dcd844
Ian Travers <mr.itpe@gmail.com>
2022-10-05 10:20:52 +0300

    26.19 Fixing Extra Bug I (pagination issue) (VI)

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.test.js
@@ -3,10 +3,11 @@ import { mount } from "@vue/test-utils";
 import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes";
 
 describe("JobFiltersSidebarJobTypes", () => {
-  const createConfig = ($store) => ({
+  const createConfig = ($store, $router) => ({
     global: {
       mocks: {
         $store,
+        $router,
       },
       stubs: {
         FontAwesomeIcon: true,
@@ -20,7 +21,11 @@ describe("JobFiltersSidebarJobTypes", () => {
         UNIQUE_JOB_TYPES: new Set(["Full-time", "Remote"]),
       },
     };
-    const wrapper = mount(JobFiltersSidebarJobTypes, createConfig($store));
+    const $router = { push: jest.fn() };
+    const wrapper = mount(
+      JobFiltersSidebarJobTypes,
+      createConfig($store, $router)
+    );
     const clickableArea = wrapper.find("[data-test='clickable-area']");
     await clickableArea.trigger("click");
 
@@ -30,22 +35,49 @@ describe("JobFiltersSidebarJobTypes", () => {
     expect(jobTypes).toEqual(["Full-time", "Remote"]);
   });
 
-  it("communicates thar user has selected checkbox for job type", async () => {
-    const commit = jest.fn();
-    const $store = {
-      getters: {
-        UNIQUE_JOB_TYPES: new Set(["Full-tyme", "Remote"]),
-      },
-      commit,
-    };
-    const wrapper = mount(JobFiltersSidebarJobTypes, createConfig($store));
-    const clickableArea = wrapper.find("[data-test='clickable-area']");
-    await clickableArea.trigger("click");
-    const remoteInput = wrapper.find("[data-test='Remote']");
-    await remoteInput.setChecked();
+  describe("when user click checkbox", () => {
+    it("communicates thar user has selected checkbox for job type", async () => {
+      const commit = jest.fn();
+      const $store = {
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-tyme", "Remote"]),
+        },
+        commit,
+      };
+      const $router = { push: jest.fn() };
+      const wrapper = mount(
+        JobFiltersSidebarJobTypes,
+        createConfig($store, $router)
+      );
+      const clickableArea = wrapper.find("[data-test='clickable-area']");
+      await clickableArea.trigger("click");
+      const remoteInput = wrapper.find("[data-test='Remote']");
+      await remoteInput.setChecked();
 
-    expect(commit).toHaveBeenLastCalledWith("ADD_SELECTED_JOB_TYPES", [
-      "Remote",
-    ]);
+      expect(commit).toHaveBeenLastCalledWith("ADD_SELECTED_JOB_TYPES", [
+        "Remote",
+      ]);
+    });
+
+    it("navigate user to job results page to see fresh batch of filtered jobs", async () => {
+      const $store = {
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-tyme", "Remote"]),
+        },
+        commit: jest.fn(),
+      };
+      const push = jest.fn();
+      const $router = { push };
+      const wrapper = mount(
+        JobFiltersSidebarJobTypes,
+        createConfig($store, $router)
+      );
+      const clickableArea = wrapper.find("[data-test='clickable-area']");
+      await clickableArea.trigger("click");
+      const remoteInput = wrapper.find("[data-test='Remote']");
+      await remoteInput.setChecked();
+
+      expect(push).toHaveBeenCalledWith({ name: "JobResults" });
+    });
   });
 });
