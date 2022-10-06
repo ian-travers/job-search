1087ddabf716c3d05eb2bf5a4d2f950eba956962
Ian Travers <mr.itpe@gmail.com>
2022-10-06 14:37:14 +0300

    27.08 Multiple Levels of Reactivity

--- src/playground.js
+++ src/playground.js
@@ -8,9 +8,12 @@ const person = reactive({
 const title = computed(
   () => `${person.firstName} ${person.lastName} the Great`
 );
+const titleLength = computed(() => title.value.length);
 
 console.log(title.value);
+console.log(titleLength.value);
 
-person.firstName = "Peter";
+person.firstName = "Oliver";
 
 console.log(title.value);
+console.log(titleLength.value);
