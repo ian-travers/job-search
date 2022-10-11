4966025703eff840024df38781507e786437a548
Ian Travers <mr.itpe@gmail.com>
2022-10-11 10:33:36 +0300

    29.12 Adding More Composables for Getters

--- src/store/composables.js
+++ src/store/composables.js
@@ -1,10 +1,26 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 
-import { FILTERED_JOBS } from "@/store//costants";
+import {
+  FILTERED_JOBS,
+  UNIQUE_JOB_TYPES,
+  UNIQUE_ORGANIZATIONS,
+} from "@/store//costants";
 
 export const useFilteredJobs = () => {
   const store = useStore();
 
   return computed(() => store.getters[FILTERED_JOBS]);
+};
+
+export const useUniqueJobTypes = () => {
+  const store = useStore();
+
+  return computed(() => store.getters[UNIQUE_JOB_TYPES]);
+};
+
+export const useUniqueOrganizations = () => {
+  const store = useStore();
+
+  return computed(() => store.getters[UNIQUE_ORGANIZATIONS]);
 };
