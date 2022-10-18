3af165022847bc8689ce63469e3a32fb952ec4bc
Ian Travers <mr.itpe@gmail.com>
2022-10-18 10:50:24 +0300

    32.20 Updating Composables Tests

--- tests/unit/store/composables.test.ts
+++ tests/unit/store/composables.test.ts
@@ -8,10 +8,12 @@ import {
   useFetchJobsDispatch,
 } from "@/store/composables";
 
+const useStoreMock = useStore as jest.Mock;
+
 describe("composables", () => {
   describe("useFilteredJobs", () => {
     it("retrieves filtered jobs from store", () => {
-      useStore.mockReturnValue({
+      useStoreMock.mockReturnValue({
         getters: {
           FILTERED_JOBS: [{ id: 1 }],
         },
@@ -25,7 +27,7 @@ describe("composables", () => {
 
   describe("useUniqueJobTypes", () => {
     it("retrieves unique job types from store", () => {
-      useStore.mockReturnValue({
+      useStoreMock.mockReturnValue({
         getters: {
           UNIQUE_JOB_TYPES: ["Full-time", "Remote"],
         },
@@ -39,7 +41,7 @@ describe("composables", () => {
 
   describe("useUniqueOrganizations", () => {
     it("retrieves unique organizations from store", () => {
-      useStore.mockReturnValue({
+      useStoreMock.mockReturnValue({
         getters: {
           UNIQUE_ORGANIZATIONS: ["Google", "Amazon"],
         },
@@ -54,7 +56,7 @@ describe("composables", () => {
   describe("useFetchJobsDispatch", () => {
     it("sends call to fetch jobs from API", () => {
       const dispatch = jest.fn();
-      useStore.mockReturnValue({
+      useStoreMock.mockReturnValue({
         dispatch,
       });
 
