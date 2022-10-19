304a5d0948c42215efc18e0d43d0c89a1cc96d1c
Ian Travers <mr.itpe@gmail.com>
2022-10-19 09:55:45 +0300

    33.10 Adding Types to Composables II (VI)

--- src/composables/usePreviousAndNextPages.ts
+++ src/composables/usePreviousAndNextPages.ts
@@ -1,6 +1,13 @@
 import { computed } from "vue";
 
-const usePreviousAndNexpPages = (currentPage, maxPage) => {
+interface SimpleRef {
+  value: number;
+}
+
+const usePreviousAndNexpPages = (
+  currentPage: SimpleRef,
+  maxPage: SimpleRef
+) => {
   const previousPage = computed(() => {
     const previousPage = currentPage.value - 1;
     const firstPage = 1;
