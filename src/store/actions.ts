38b0912bea015c494438b641ce5b386c8bcca7ea
Ian Travers <mr.itpe@gmail.com>
2022-10-24 10:12:43 +0300

    34.07 TDD for Actions FETCH DEGREES

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,7 +1,13 @@
 import { Commit } from "vuex";
 
-import { FETCH_JOBS, RECEIVE_JOBS } from "@/store/costants";
+import {
+  FETCH_JOBS,
+  RECEIVE_DEGREES,
+  FETCH_DEGREES,
+  RECEIVE_JOBS,
+} from "@/store/costants";
 import getJobs from "@/api/getJobs";
+import getDegrees from "@/api/getDegrees";
 
 interface Context {
   commit: Commit;
@@ -11,6 +17,11 @@ export const actions = {
   [FETCH_JOBS]: async (context: Context) => {
     const jobListings = await getJobs();
     context.commit(RECEIVE_JOBS, jobListings);
+  },
+
+  [FETCH_DEGREES]: async (context: Context) => {
+    const degreess = await getDegrees();
+    context.commit(RECEIVE_DEGREES, degreess);
   },
 };
 
