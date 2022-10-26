797b2c66a00167d9488758d6543d8c7218517bb0
Ian Travers <mr.itpe@gmail.com>
2022-10-26 16:04:11 +0300

    35.03 Wiring up Click Handler In JobFiltersSidebar (wip)

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -4,7 +4,11 @@
       <div class="flex justify-between items-baseline">
         <h3 class="text-base font-semibold my-4">What do you want to do?</h3>
         <div class="text-sm">
-          <action-button text="Clear Filters" type="secondary" />
+          <action-button
+            text="Clear Filters"
+            type="secondary"
+            @click="clearUserJobFilterSelections"
+          />
         </div>
       </div>
 
@@ -34,6 +38,9 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useStore } from "vuex";
+
+import { key } from "@/store";
 
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
@@ -47,6 +54,7 @@ import {
   ADD_SELECTED_JOB_TYPES,
   ADD_SELECTED_ORGANIZATIONS,
   ADD_SELECTED_DEGREES,
+  CLEAR_USER_JOB_FILTER_SELECTIONS,
 } from "@/store/costants";
 
 export default defineComponent({
@@ -58,9 +66,14 @@ export default defineComponent({
   },
 
   setup() {
+    const store = useStore(key);
     const uniqueJobTypes = useUniqueJobTypes();
     const uniqueOrganizations = useUniqueOrganizations();
     const uniqueDegrees = useUniqueDegrees();
+
+    const clearUserJobFilterSelections = () => {
+      store.commit(CLEAR_USER_JOB_FILTER_SELECTIONS);
+    };
 
     return {
       uniqueJobTypes,
@@ -69,6 +82,7 @@ export default defineComponent({
       ADD_SELECTED_JOB_TYPES,
       ADD_SELECTED_ORGANIZATIONS,
       ADD_SELECTED_DEGREES,
+      clearUserJobFilterSelections,
     };
   },
 });
