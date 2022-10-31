dd05909487aae57b20486372f71fe357b8acad8f
Ian Travers <mr.itpe@gmail.com>
2022-10-31 09:36:13 +0300

    36.16 Fixing Failing Tests for JobFiltersSidebar Component

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.test.ts
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.test.ts
@@ -1,12 +1,36 @@
 import { shallowMount, VueWrapper } from "@vue/test-utils";
+
+import { useStore } from "vuex";
 jest.mock("vuex");
+const useStoreMock = useStore as jest.Mock;
+
+import { useRoute } from "vue-router";
+jest.mock("vue-router");
+const useRouteMock = useRoute as jest.Mock;
 
 import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
 
 describe("JobFiltersSidebar", () => {
   it("sets up panel for filtering features", () => {
+    useStoreMock.mockReturnValue({ commit: jest.fn() });
+    useRouteMock.mockReturnValue({
+      query: {},
+    });
     const wrapper = shallowMount(JobFiltersSidebar);
 
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it("reads query params to filter initial jobs for user", () => {
+    const commit = jest.fn();
+    useStoreMock.mockReturnValue({ commit });
+    useRouteMock.mockReturnValue({
+      query: {
+        role: "Vue",
+      },
+    });
+    shallowMount(JobFiltersSidebar);
+
+    expect(commit).toHaveBeenCalledWith("UPDATE_SKILLS_SEARCH_TERM", "Vue");
   });
 });
