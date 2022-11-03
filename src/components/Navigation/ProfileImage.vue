154b85d511567ecf1048137d1918bdbd3fd20e00
Ian Travers <mr.itpe@gmail.com>
2022-11-03 09:17:21 +0300

    Update ProfileImage

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -2,16 +2,7 @@
   <img :src="imageUrl" class="w-8 h-8 object-contain rounded-3xl" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "ProfileImage",
-  data() {
-    return {
-      imageUrl:
-        "https://www.pngitem.com/pimgs/m/487-4876417_link-head-png-toon-link-face-png-transparent.png",
-    };
-  },
-});
+<script setup lang="ts">
+const imageUrl =
+  "https://www.pngitem.com/pimgs/m/487-4876417_link-head-png-toon-link-face-png-transparent.png";
 </script>
