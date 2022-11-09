e7871bc02ac11b55554dd8f471cc3848add5ad2f
Ian Travers <mr.itpe@gmail.com>
2022-11-09 09:49:35 +0300

    Install and setup pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -16,6 +17,7 @@ library.add(faAngleDown, faAngleUp, faSearch);
 
 createApp(App)
   .use(store, key)
+  .use(createPinia())
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
