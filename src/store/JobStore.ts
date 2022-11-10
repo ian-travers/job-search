f12146f5f9c8f10af12292b253987e226f1deadb
Ian Travers <mr.itpe@gmail.com>
2022-11-10 14:48:42 +0300

    Add FILTERED_JOBS getter (with exptra parameter) (VI)

--- src/store/JobStore.ts
+++ src/store/JobStore.ts
@@ -10,7 +10,17 @@ import {
   INCLUDE_JOB_BY_JOB_TYPE,
   INCLUDE_JOB_BY_DEGREE,
   INCLUDE_JOB_BY_SKILL,
+  FILTERED_JOBS,
 } from "@/store/costants";
+import state from "./state";
+import getters from "./getters";
+
+interface IncludeJobGetters {
+  INCLUDE_JOB_BY_ORGANIZATION: (job: Job) => boolean;
+  INCLUDE_JOB_BY_JOB_TYPE: (job: Job) => boolean;
+  INCLUDE_JOB_BY_DEGREE: (job: Job) => boolean;
+  INCLUDE_JOB_BY_SKILL: (job: Job) => boolean;
+}
 
 export const useJobStore = defineStore("JobStore", {
   state: () => ({
@@ -39,29 +49,35 @@ export const useJobStore = defineStore("JobStore", {
     [UNIQUE_DEGREES](state: JobState) {
       return state.degrees.map((degree) => degree.degree);
     },
+    [INCLUDE_JOB_BY_ORGANIZATION](state: JobState) {
+      (job: Job) => {
+        if (state.selectedOrganizations.length === 0) return true;
 
-    [INCLUDE_JOB_BY_ORGANIZATION]: (state: JobState) => (job: Job) => {
-      if (state.selectedOrganizations.length === 0) return true;
-
-      return state.selectedOrganizations.includes(job.organization);
+        return state.selectedOrganizations.includes(job.organization);
+      };
     },
-
     [INCLUDE_JOB_BY_JOB_TYPE]: (state: JobState) => (job: Job) => {
       if (state.selectedJobTypes.length === 0) return true;
 
       return state.selectedJobTypes.includes(job.jobType);
     },
-
     [INCLUDE_JOB_BY_DEGREE]: (state: JobState) => (job: Job) => {
       if (state.selectedDegrees.length === 0) return true;
 
       return state.selectedDegrees.includes(job.degree);
     },
-
     [INCLUDE_JOB_BY_SKILL]: (state: JobState) => (job: Job) => {
       return job.title
         .toLocaleLowerCase()
         .includes(state.skillsSearchTerm.toLocaleLowerCase());
+    },
+    [FILTERED_JOBS](state: JobState) {
+      return (getters: IncludeJobGetters) =>
+        state.jobs
+          .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
+          .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job))
+          .filter((job) => getters.INCLUDE_JOB_BY_DEGREE(job))
+          .filter((job) => getters.INCLUDE_JOB_BY_SKILL(job));
     },
   },
 });
